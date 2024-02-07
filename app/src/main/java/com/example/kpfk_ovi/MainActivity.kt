396206cb6b7107c5eb0a3da75c8e8ca96fbc0fc6
VolodymyrOn <saharokword@gmail.com>
2024-02-07 20:18:38 +0200

    First

---
+++ app/src/main/java/com/example/kpfk_ovi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kpfk_ovi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
